0370d62f93ac63126833807915a393aa3fbf0963
1x2kb <natemeyer@protonmail.com>
2022-06-20 21:15:00 -0500

    Added more specific returns for various unhappy path situations.

--- routes/eth-lookup.js
+++ routes/eth-lookup.js
@@ -16,10 +16,27 @@ const ETH_API_SERVER =
  */
 const memoryCache = {};
 
+class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+
+    this.name = 'NotFoundError';
+    this.code = 404;
+  }
+}
+
 async function doLookup(name) {
   const provider = new providers.JsonRpcProvider(ETH_API_SERVER);
   const resolver = await provider.getResolver(name);
+  if (!resolver) {
+    throw new NotFoundError("Name provided was not found");
+  }
+
   const url = await resolver.getText('phone');
+
+  if (!url) {
+    throw new NotFoundError("User did not have a phone number");
+  }
   return url;
 }
 
@@ -31,7 +48,6 @@ function saveNameUrl(name, url) {
   }
 }
 
-/** */
 async function getUrl(name) {
   const nameFromCache = memoryCache[name];
   if (nameFromCache && nameFromCache.goodTill > Date.now()) {
@@ -39,12 +55,14 @@ async function getUrl(name) {
   }
 
   const url = await doLookup(name);
-  saveNameUrl(name, url);
+  if (url) 
+    saveNameUrl(name, url);
 
   return url;
 }
 
 router.get("/", async (request, response) => {
+  try {
     const name = request.query.name;
 
     if (name && name != '') {
@@ -52,8 +70,15 @@ router.get("/", async (request, response) => {
         response.setHeader("Content-Type", "text/plain")
         return response.status(200).send(url);
     }
-      
-    return response.status(404).send("Name was not found");
+
+    return response.status(400).send({message: "Name was not provided. Name is a required query param."});
+  } catch(e) {
+    if (e instanceof NotFoundError) {
+      return response.status(e.code).send(e.message)
+    }
+  }
+
+  return response.status(500).send("Unexpected error occurred");
 });
 
 module.exports = router;
