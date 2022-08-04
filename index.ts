1ea866924212219543b7e03c7d93d838e8989098
bradrf <bradrf@enum.wtf>
2022-08-04 14:48:19 -0700

    conform to generating for esm compatible javascript (ugh!)

--- index.ts
+++ index.ts
@@ -6,49 +6,49 @@
 
  import dbg from 'debug';
  import http from 'http';
- 
- import app from './app';
- 
+
+ import app from './app.js';
+
  const debug = dbg('ether-cache:server');
- 
+
  /**
   * Normalize a port into a number, string, or false.
   */
- 
+
  function normalizePort(val: string) {
    const port = parseInt(val, 10);
- 
+
    if (Number.isNaN(port)) {
      // named pipe
      return val;
    }
- 
+
    if (port >= 0) {
      // port number
      return port;
    }
- 
+
    return false;
  }
- 
+
  /**
   * Get port from environment and store in Express.
   */
- 
+
  const port = normalizePort(process.env.PORT || '3000');
  app.set('port', port);
- 
+
  /**
   * Event listener for HTTP server "error" event.
   */
- 
+
  function onError(error: NodeJS.ErrnoException) {
    if (error.syscall !== 'listen') {
      throw error;
    }
- 
+
    const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
- 
+
    // handle specific listen errors with friendly messages
    switch (error.code) {
      case 'EACCES':
@@ -63,27 +63,27 @@
        throw error;
    }
  }
- 
+
  /**
   * Create HTTP server.
   */
- 
+
  const server = http.createServer(app);
- 
+
  /**
   * Event listener for HTTP server "listening" event.
   */
- 
+
  function onListening() {
    const addr = server.address();
    const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr?.port}`;
    debug(`Listening on ${bind}`);
  }
- 
+
  /**
   * Listen on provided port, on all network interfaces.
   */
- 
+
  server.listen(port);
  server.on('error', onError);
  server.on('listening', onListening);
